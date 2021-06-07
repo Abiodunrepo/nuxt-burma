b3a54616ed95eeb10271ef56d554a566a85995b1
unknown <abiodundanielrepo@gmail.com>
2021-06-07 16:21:40 +0800

    t remote add origin https://github.com/Abiodunrepo/nuxt-burma.git s Please enter the commit message for your changes. Lines starting

---
+++ components/He.vue
@@ -0,0 +1,26 @@
+<template>
+  
+  <div class="section">
+    <div class="container">
+      <div class="columns">
+      <div class="column has-text-centered">
+        hello
+      </div>
+      <div class="column has-text-centered">
+        hello
+      </div>
+      </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  <He/>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+
+
